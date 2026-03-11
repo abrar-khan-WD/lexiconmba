cff6ea1205f2f2dee3c581608f809abb34c46b53
SHUBHAM SHIVAJI LINGAYAT <115860658+shubham-lingayat@users.noreply.github.com>
2026-03-11 19:10:03 +0530

    shubh 01

--- GlobalMBA/src/components/HeroMobile.jsx
+++ GlobalMBA/src/components/HeroMobile.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import bgImage from "../assets/hero/hero-mobile.png"
 import "./HeroDesktop.css"
 import {NpfWidget} from "./npfWidget"
 
 export const HeroMobile = () => {
     return (
        <div className = "hero-mobile flex flex-col text-center min-h-screen gap-4">
-         <img src= {bgImage} alt="Image Not Found" />
+         <img src="./hero/2.jpg" alt="Image Not Found" />
          <div className="px-2">
           <div className="p-4 border rounded-md">
             <NpfWidget />
